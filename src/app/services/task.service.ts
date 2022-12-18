2e5e5c66cc7b00c8f912d7c78fc0b35b4ee2e6cd
mezez <ekemammezez@gmail.com>
2022-12-18 07:02:37 +0100

    fetch tasks from json server (BE) with httpclient module

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 import {Observable, of} from 'rxjs';
 import { Task } from 'src/app/Task';
 import { TASKS } from 'src/app/mock-tasks';
@@ -7,11 +8,17 @@ import { TASKS } from 'src/app/mock-tasks';
   providedIn: 'root'
 })
 export class TaskService {
+  private apiUrl: string = 'http://localhost:5000/tasks'
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
-  getTasks(): Observable<Task[]>{
+  getTasksFromFile(): Observable<Task[]>{
     const tasks = of(TASKS);
     return tasks
+  }
+
+  //fetch tasks from an actual backend
+  getTasks(): Observable<Task[]>{
+    return this.http.get<Task[]>(this.apiUrl)
   }
 }
